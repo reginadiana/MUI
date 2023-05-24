40a4626fa60eecc8deee854ba544b75d14c575c4
Diana Regina <diana.silva@rebase.com.br>
2023-05-23 21:43:06 -0300

    add icons lib, use grid and alert components

--- src/App.js
+++ src/App.js
@@ -1,27 +1,142 @@
+import { useState } from "react";
 import "./App.css";
-import Button from "@mui/material/Button";
-import Box from "@mui/material/Box";
+
+import Alert from "@mui/material/Alert";
+import AlertTitle from "@mui/material/AlertTitle";
 import Avatar from "@mui/material/Avatar";
+import Box from "@mui/material/Box";
+import Button from "@mui/material/Button";
+import CloseIcon from "@mui/icons-material/Close";
+import CheckIcon from "@mui/icons-material/Check";
+import Collapse from "@mui/material/Collapse";
+import Grid from "@mui/material/Grid";
+import IconButton from "@mui/material/IconButton";
+import LinearProgress from "@mui/material/LinearProgress";
 import Stack from "@mui/material/Stack";
 import Typography from "@mui/material/Typography";
-import LinearProgress from "@mui/material/LinearProgress";
-import Alert from '@mui/material/Alert';
 
 function App() {
+  const [open, setOpen] = useState(true);
+
   return (
     <Box>
       <Box sx={{ m: 8, p: 2, border: "1px dashed grey" }}>
-        <Stack spacing={2}>
-          <Alert severity="error">This is an error alert — check it out!</Alert>
-          <Alert severity="warning">
-            This is a warning alert — check it out!
-          </Alert>
-          <Alert severity="info">This is an info alert — check it out!</Alert>
-          <Alert severity="success">
-            This is a success alert — check it out!
-          </Alert>
-        </Stack>
+        <Grid container rowSpacing={1} columnSpacing={{ xs: 1, sm: 2, md: 3 }}>
+          <Grid item xs={6}>
+            <Alert severity="success">Mensagem de sucesso (padrão)</Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert variant="outlined" severity="error">Com variant outlined</Alert>
+          </Grid>
+
+
+          <Grid item xs={6}>
+            <Alert variant="filled" severity="warning">Com variant filled</Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert severity="error">Mensagem de erro</Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert severity="warning">Mensagem de alerta</Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert severity="info">Mensagem de informação</Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert onClose={() => alert("onClose callback was trigged")}>
+              Click there!
+            </Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Collapse in={open}>
+              <Alert
+                action={
+                  <IconButton
+                    aria-label="close"
+                    color="inherit"
+                    size="small"
+                    onClick={() => setOpen(false)}
+                  >
+                    <CloseIcon fontSize="inherit" />
+                  </IconButton>
+                }
+              >
+                Close me! I'm using collapse transition
+              </Alert>
+            </Collapse>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert
+              action={
+                <Button
+                  onClick={() =>
+                    alert("onClick callback was trigged by action button alert")
+                  }
+                  color="info"
+                  size="large"
+                >
+                  UNDO
+                </Button>
+              }
+              severity="info"
+            >
+              Ação customizada
+            </Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Button
+              disabled={open}
+              variant="outlined"
+              onClick={() => {
+                setOpen(true);
+              }}
+            >
+              Re-open
+            </Button>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert icon={false} severity="success">
+              Alerta sem icone
+            </Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert icon={<CheckIcon fontSize="inherit" />} severity="warning">
+              Icone customizado
+            </Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert
+              iconMapping={{
+                error: <CheckIcon fontSize="inherit" />,
+              }}
+              severity="error"
+            >
+              <AlertTitle>Alerta com iconMapping</AlertTitle>
+              Permite customizar um icone para cada tipo de severity, tornando o
+              componente mais dinamico
+            </Alert>
+          </Grid>
+
+          <Grid item xs={6}>
+            <Alert severity="error">
+              <AlertTitle>Titulo</AlertTitle>Mensagem -{" "}
+              <strong>importante!</strong>
+            </Alert>
+          </Grid>
+        </Grid>
       </Box>
+
       <Box sx={{ m: 8, p: 2, border: "1px dashed grey" }}>
         <Stack spacing={2}>
           <LinearProgress />
@@ -30,6 +145,7 @@ function App() {
           <LinearProgress color="inherit" />
         </Stack>
       </Box>
+
       <Box sx={{ m: 8, p: 2, border: "1px dashed grey" }}>
         <Stack direction="row" useFlexGap flexWrap="wrap" spacing={2}>
           <Button variant="text">Text</Button>
